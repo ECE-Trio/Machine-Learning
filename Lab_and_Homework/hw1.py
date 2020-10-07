da488e3a115434c4544c768e7145baca5dc7d0e4
NeilSCGH <neil.segard@gmail.com>
2020-10-07 11:09:14 +0200

    bug fixed

--- Lab_and_Homework/hw1.py
+++ Lab_and_Homework/hw1.py
@@ -16,13 +16,10 @@ log("\t{} messages\n".format(N))
 ##Binary change
 # 1 if spam, 0 if not spam
 for value in messages:
-    if (value[0]=="spam"):
-        value[0]=1
-    else:
-        value[0]=0
+    value[0] = (value[0]=="spam")*1
 
 ##Splitting train and test data
-log("\nSplitting data... ")
+log("\nQuestion 1: Splitting data... ")
 trainingSize = int(N * trainingProportion)
 testSize = N - trainingSize
 
@@ -34,7 +31,7 @@ log("\t{} messages for training\n".format(trainingSize))
 log("\t{} messages for testing\n".format(testSize))
 
 ##Making the dictionary
-log("\nMaking dictionary... ")
+log("\nQuestion 3: Making dictionary... ")
 
 listChar="[\'!\"$%&'()*+,-./:;<=>?@[\\]^_|~¡£¥¦¨©¬º»¼¾âãéœˆ˜–‘’“”‰€™],"
 def cleanMsg(msg):
@@ -78,7 +75,7 @@ def extractFeaturesMatrix(set):
 
     return features_matrix
 
-log("\nExtracting features :\n")
+log("\nQuestion 4: Extracting features:\n")
 log("\tTraining set... ")
 trainingFeaturesMatrix=extractFeaturesMatrix(trainingSet)
 log("ok\n")
@@ -87,7 +84,7 @@ testFeaturesMatrix=extractFeaturesMatrix(testSet)
 log("ok\n")
 
 ##Fitting Naives Bayes
-log("\nFitting Naives Bayes... ")
+log("\nQuestion 5: Fitting Naives Bayes... ")
 countWordsInSpam=np.zeros(nbWords)
 countWordsInHam=np.zeros(nbWords)
 counterSpam=0
@@ -105,16 +102,16 @@ phi=counterSpam/trainingSize
 log("ok\n")
 
 ##Testing
-log("\nTesting... ")
-alpha=0.000000000000000001
+log("\nQuestion 6: Testing... ")
+alpha=0.00000000000001
 predictList=[]
 
 for type, message in testSet:
     content = cleanMsg(message)
     words=content.split(" ")
     words=np.unique(np.asarray(words))
-    sumInSpam=0
-    sumInHam=0
+    sumInSpam=1
+    sumInHam=1
 
     for word in words:
         try:
@@ -123,22 +120,24 @@ for type, message in testSet:
             sumInHam *= countWordsInHam[wordIndexInDict]
         except: pass #word not in dictionary
 
-    p = (sumInSpam * phi + alpha) / (sumInHam * (1-phi) + sumInSpam * phi + 2*alpha)
+    probaSpam = (sumInSpam * phi + alpha) / (sumInHam * (1-phi) + sumInSpam * phi + 2*alpha)
+    probaHam = (sumInHam * (1-phi) + alpha) / (sumInHam * (1-phi) + sumInSpam * phi + 2*alpha)
 
-    predictList.append(p)
+    predictList.append(probaSpam>probaHam)
 
 predictList=np.asarray(predictList)
 log("ok\n")
 
-#Measuring performance
-log("\nMeasuring performance... ")
+##Measuring performance
+log("\nQuestion 7: Measuring performance... ")
 
 def positive_negative(predict,test):
     true_positive = 0 #predicted spam and is spam
     false_positive = 0 #predicted spam and is not spam
-    true_negative = 0 # predicted not spam and is not spam
-    false_negative = 0 # predicted not spam and is spam
+    true_negative = 0 #predicted not spam and is not spam
+    false_negative = 0 #predicted not spam and is spam
     N = len(predict)
+
     for i in range(N):
         if (predict[i] and test[i][0] == "1"):
             true_positive+=1
@@ -150,7 +149,7 @@ def positive_negative(predict,test):
             false_negative+=1
     return true_positive, false_positive, true_negative, false_negative
 
-true_positive, false_positive, true_negative, false_negative = positive_negative(predictList>0.5,testSet)
+true_positive, false_positive, true_negative, false_negative = positive_negative(predictList,testSet)
 log("ok\n")
 
 print()
@@ -158,10 +157,15 @@ print("           \tSpam  \t  Ham")
 print("Pred Spam  \t {}   \t   {}".format(true_positive,false_positive))
 print("Pred Ham   \t {}   \t   {}".format(false_negative,true_negative))
 
+print()
+print("Correct:  {}/{} ({}%)".format(true_positive+true_negative,testSize, round((true_positive+true_negative)/testSize*100)))
+print("Incorrect: {}/{} ({}%)".format(false_negative+false_positive,testSize, round((false_negative+false_positive)/testSize*100)))
 
 
-
-
+"""
+Question 8
+Discuss your results ☺
+"""
 
 
 
