1c7a6b65c643bd6094f2f250c3a85720abcb8e35
NeilSCGH <neil.segard@gmail.com>
2020-10-12 20:46:38 +0200

    e maj

--- Section 2/Lab_and_Homework/hw1 - from scratch.py
+++ Section 2/Lab_and_Homework/hw1 - from scratch.py
@@ -18,10 +18,40 @@ stretched_gaussian = np.dot(np.random.randn(n_samples, 3), C)
 # concatenate the two datasets into the final training set
 X_train = np.vstack([shifted_gaussian, stretched_gaussian])
 
-# fit a Gaussian Mixture Model with two components
-clf = mixture.GaussianMixture(n_components=3, covariance_type='full')
-clf.fit(X_train)
+#Initialization
+J=3
+I=len(X_train)
+phi=np.ones(J)*(1/J)
+mu=np.zeros((I,J))
+sigma=np.zeros((J,I,I))
 
+for s in sigma:
+    for i in range(I):
+        while s[i][i]==0: #to avoid having 0 in the diagonaleuhs
+            s[i][i]=1#np.random.rand()
+
+
+
+W=np.zeros((I,J))
+tab=np.zeros((I,J))
+for j in range(J):
+    det = np.sqrt(np.linalg.det(sigma[j]))*(2*np.pi)**(I/2)
+    a = (X_train.T[j] - mu.T[j]).reshape(600,1)
+    b = np.linalg.inv(sigma[j])
+    e=-0.5* np.dot(np.dot(a.T, b), a)
+    c = np.exp(e)
+    d = c * phi[j] / det
+    for i in range(I):
+        tab[i][j] = d
+
+
+for i in range(I):
+    s=np.sum(tab[i])
+    for j in range(J):
+        W[i][j] = tab[i][j] / s
+
+
+"""
 # display predicted scores by the model as a contour plot
 x = np.linspace(-20., 30., num = 7)
 y = np.linspace(-20., 40., num = 7)
@@ -48,3 +78,4 @@ ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
 plt.show()
+"""
