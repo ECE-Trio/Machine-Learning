84f1d17b2a849828c9d44cabe5af30ba479cfc55
NeilSCGH <neil.segard@gmail.com>
2020-10-21 15:44:05 +0200

    une grosse partie de faite

--- Section 3/hw_3-from-scratch.py
+++ Section 3/hw_3-from-scratch.py
@@ -14,16 +14,49 @@ from matplotlib.colors import LogNorm
 from sklearn import mixture
 
 # Variables
-N = 4 #number of hidden states Z including z0
-K = 5 #number of hidden states X including x0
+Z = [0,1,2,3]
+X = [0,1,2,3,4]
+X_target = [1,3,2,0]
 
-a_ij = np.zeros((N,N))
-b_jk = np.zeros((N,K))
+a=np.asarray([[1.,  0.,  0.,  0.], #Size of a : NxN
+             [0.2, 0.3, 0.1, 0.4],
+             [0.2, 0.5, 0.2, 0.1],
+             [0.7, 0.1, 0.1, 0.1]])
 
-X= [1,3,2,0]
-T=len(X)
+b=np.asarray([[1., 0.,  0.,  0.,  0.], #Size of b : NxK
+             [0., 0.3, 0.4, 0.1, 0.2],
+             [0., 0.1, 0.1, 0.7, 0.1],
+             [0., 0.5, 0.2, 0.1, 0.2]])
 
-path = [1] #We suppose at time t=0 we are at Z1
+def calculAlpha(Z, X, a, b, X_target):
+    N = len(Z) #number of hidden states Z including z0
+    K = len(X) #number of hidden states X including x0
+    T=len(X_target)
+
+    path = [1] #We suppose at time t=0 we are at Z1
+    alpha=np.zeros((N,T+1))
+    alpha[path[0]][0]=1
+
+
+    for t in range(1, T+1):
+        for j in range(N):
+            sum=0
+            for i in range(N):
+                sum+= alpha[i][t-1] * a[i][j]
+
+            k=X_target[t-1]
+            alpha[j][t] = sum * b[j][k]
+
+        jMax = np.argmax(alpha.T[t])
+        path.append(jMax)
+
+    return path
+
+print(calculAlpha(Z, X, a, b, X_target))
+
+
+
+
 
 def alpha_j(j,t):
     sum=0
